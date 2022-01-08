0a150d782a9722e7377d5932fa72e0de7ed1724a
Gurbet Güngören <81263610+GurbetGungoren@users.noreply.github.com>
2022-01-08 17:54:14 +0300

    book search part is complete
    book search part is complete, user actions will be added

---
+++ BigLiblary Project/src/main/java/com/controller/USerBooksController.java
@@ -0,0 +1,12 @@
+package com.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+
+public class USerBooksController {
+
+
+    @PostMapping("/addbook")
+    public String addBookForUser(){
+
+    }
+}
